ba2dcffd366e93772c0a6fa250e71edb108e0fae
tejuypatil <tejuypatil@gmail.com>
2022-07-14 15:08:44 +0530

    Added program by using use case 1

--- src/com/bridgelabz/game/TicTacToeGame.java
+++ src/com/bridgelabz/game/TicTacToeGame.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.game;
 
 public class TicTacToeGame {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe Game");
+    public static void printBoard(char[][]board)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+                for (int j = 0; j < 5; j++) {
+                    System.out.print(board[i][j]);
+                }
+                System.out.println();
+        }
     }
+    public static void main(String[] args)
+        {
+            System.out.println("Welcome to Tic Tac Toe Game");
+            char[][] board={{' ','|',' ','|',' '},
+                    {'-','+','-','+','-'},
+                    {' ','|',' ','|',' '},
+                    {'-','+','-','+','-'},
+                    {' ','|',' ','|',' '}};
+            printBoard(board);
+        }
 }
