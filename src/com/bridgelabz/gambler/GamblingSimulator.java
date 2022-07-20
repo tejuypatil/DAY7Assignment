910464117fc54c1aca82bc432e5fa30a670a671d
tejuypatil <tejuypatil@gmail.com>
2022-07-21 00:30:15 +0530

    Added program to check win or loss

--- src/com/bridgelabz/gambler/GamblingSimulator.java
+++ src/com/bridgelabz/gambler/GamblingSimulator.java
@@ -5,7 +5,24 @@ public class GamblingSimulator {
     static int BET_PER_GAME = 1;
     static int DAILY_STAKE = 100;
 
+    static  int win= 1;
+
+    static void checkWin(){
+        int random=(int)(Math.random()*10)%2;
+        if(random==win){
+            DAILY_STAKE++;
+            System.out.println("You win ");
+        }
+        else{
+            DAILY_STAKE--;
+            System.out.println("You loose");
+
+        }
+        System.out.println("You have a Daily Stake = "+DAILY_STAKE);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
+        checkWin();
     }
 }
